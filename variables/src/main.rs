a61a6b36d722461a981d43551778210a1d5114f7
anxielray <anxielworld@gmail.com>
2025-03-24 07:59:56 -0400

    [Variables]: |Sourcefile| {Add some kex comments for future referrence in the future for learning variables}

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,21 @@
 fn main() {
-   let x: i32 = 400;
-   let y: f64 = 907.54;
+   let x: i32 = 400; // i8, i16, i32, i64, i128
+   /*
+   u8:
+   2^8 unique values
+   0 -2^8-1
+   i8
+   -2^7 - 2^7-1
+   (-128 - 127)
+   */
+   let y: f64 = 907.54; // f32
    let c: char = 'a';
-   let tup: (i32, bool, char) = (2, true, 'x');
-   let arr: [i32; 5] = [4, 5, 7, 8, 2];
+   // booleans can be represented using 1 or 0
+   
+   //=======( compound types
+   //
+   let tup: (i32, bool, char) = (2, true, 'x');// has fixed leangth
+   let arr: [i32; 5] = [4, 5, 7, 8, 2];// cannot add elements
    println!("{}", x);
    println!("{}", y);
    println!("{}", c);
