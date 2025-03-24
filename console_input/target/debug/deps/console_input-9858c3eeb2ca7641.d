cf0f8da1768e2ea0ae79a6472297eb2ef4e95bac
anxielray <anxielworld@gmail.com>
2025-03-24 08:25:54 -0400

    [Console_input]: |binaries| { Add some of the binaries used in cargo project manager for the project console input}

---
+++ console_input/target/debug/deps/console_input-9858c3eeb2ca7641.d
@@ -0,0 +1,5 @@
+/home/anxiel/projects/github/rusty_models/console_input/target/debug/deps/console_input-9858c3eeb2ca7641: src/main.rs
+
+/home/anxiel/projects/github/rusty_models/console_input/target/debug/deps/console_input-9858c3eeb2ca7641.d: src/main.rs
+
+src/main.rs:
