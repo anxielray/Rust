5763d4c48ad1dca30241b428c422acf6ea5b08b2
anxielray <anxielworld@gmail.com>
2025-03-24 07:29:33 -0400

    [Project]: |Cargo| {Add some of the files generated by cargo used in the project management}

---
+++ topic_one/target/debug/deps/tutorial_one-7ff1deb59fb3cada.d
@@ -0,0 +1,5 @@
+/home/anxiel/projects/github/rusty_models/tutorial_one/target/debug/deps/tutorial_one-7ff1deb59fb3cada: src/main.rs
+
+/home/anxiel/projects/github/rusty_models/tutorial_one/target/debug/deps/tutorial_one-7ff1deb59fb3cada.d: src/main.rs
+
+src/main.rs:
---
+++ topic_one/target/debug/deps/tutorial_one-d96c543c9e639d1d.d
@@ -0,0 +1,5 @@
+/home/anxiel/projects/github/rusty_models/tutorial_one/target/debug/deps/libtutorial_one-d96c543c9e639d1d.rmeta: src/main.rs
+
+/home/anxiel/projects/github/rusty_models/tutorial_one/target/debug/deps/tutorial_one-d96c543c9e639d1d.d: src/main.rs
+
+src/main.rs:
