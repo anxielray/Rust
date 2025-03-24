86934a974e6e38435a2b3747324d5d43687bc3c6
anxielray <anxielworld@gmail.com>
2025-03-24 09:21:21 -0400

    [Sourcefile]: Add the project to learn about the conditional statements in Rust programming language. Learn about the order of logical operators. Learn that  when using arithmetics comparison operators, we can only use them on variables with the same types

---
+++ conditions_control_flows/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let food = "pizza";
+
+    if food == "cookie"{
+        println!("I like cookies too!");
+    } else if food == "pizza" {
+        println!("what size?")
+    } else {
+        println!("Maybe I should rethink my favorite!")
+    }
+}
+
+//we cannot use different types;
+//order : ===(  ! && ||
---
+++ conditions_control_flows/target/debug/conditions_control_flows.d
@@ -0,0 +1,1 @@
+/home/anxiel/projects/github/rusty_models/conditions_control_flows/target/debug/conditions_control_flows: /home/anxiel/projects/github/rusty_models/conditions_control_flows/src/main.rs
