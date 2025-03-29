dd7db2d3d8f736a4489e09caf81bc73d68f66f9a
anxielray <anxielworld@gmail.com>
2025-03-29 07:17:40 -0400

    [Binaries]{Add binaries used in the rust cargo building management}

---
+++ tut_2/target/debug/deps/tut_2-14cb821d71696e85.d
@@ -0,0 +1,5 @@
+/home/anxiel/projects/github/rusty_models/tut_2/target/debug/deps/libtut_2-14cb821d71696e85.rmeta: src/main.rs
+
+/home/anxiel/projects/github/rusty_models/tut_2/target/debug/deps/tut_2-14cb821d71696e85.d: src/main.rs
+
+src/main.rs:
---
+++ tut_2/target/debug/deps/tut_2-bb83ce8652b21fb1.d
@@ -0,0 +1,5 @@
+/home/anxiel/projects/github/rusty_models/tut_2/target/debug/deps/libtut_2-bb83ce8652b21fb1.rmeta: src/main.rs
+
+/home/anxiel/projects/github/rusty_models/tut_2/target/debug/deps/tut_2-bb83ce8652b21fb1.d: src/main.rs
+
+src/main.rs:
