75e77e49b2fe0885fbb92b6a95e675f4fedc44f9
anxielray <anxielworld@gmail.com>
2025-03-29 07:37:12 -0400

    [tutorial 2]: {Modify the file for learning how to use variable shadowing}

--- tut_2/src/main.rs
+++ tut_2/src/main.rs
@@ -8,9 +8,11 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("What is your name?");
-    let mut name: String = String::new();
-    let greeting: &str = "Nice to meet you";
-    io::stdin().read_line(&mut name).expect("Did not find  input message!");
-    println!("Hello {}! {}", name.trim_end(), greeting);
+    const ONE_MIL: u32 = 1_000_000;
+    const PI: f32 = 3.141592;
+    let age: &str = "47";
+    let mut age: u32 = age.trim().parse().expect("age wasn't assigned  a number");
+    //whatever  just did from above is called shadowing; using a variable more than once. and it is acceptable to use the same variable with different data types
+    age = age+1;
+    println!("I'm {} and I want to earn ${}", age, ONE_MIL);
 }
