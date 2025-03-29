4973a2c4329be5d0876e477db6630360b29b0dab
anxielray <anxielworld@gmail.com>
2025-03-29 08:35:13 -0400

    [Loop]: {Update the sourcefile to learn how to use loops in Rust programming language}

--- tut_2/src/main.rs
+++ tut_2/src/main.rs
@@ -8,16 +8,23 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    let my_age = 18;
-    let voting_age = 18;
-    match my_age.cmp(&voting_age){
-        Ordering::Less => println!("Can't vote"),
-        Ordering::Greater => println!("Can vote"),
-        Ordering::Equal => println!("You gained the right to vote!"),
-    };
+    let arr_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let mut loop_indx = 0;
+    loop{
+        if arr_2[loop_indx] %2 == 0{
+            loop_indx += 1;
+            continue;
+        }
+        if arr_2[loop_indx] == 9{
+            break;
+        }
+        println!("Val: {}", arr_2[loop_indx]);
+        loop_indx+=1;
+    }
 }
 
 /*
 To let the rust complier to assume a variable is to start the variable in an underscore
 an f32 has 6 digits of precision while an f64 has 14 digits of precision
+arrays have a fixed size
 */
