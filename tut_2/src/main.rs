38e89c7107584482739ca6000084a642b213ef22
anxielray <anxielworld@gmail.com>
2025-03-29 08:14:04 -0400

    [Conditions]: |if else| {Update the sourcefile to learn the if else condition in Rust programming language}

--- tut_2/src/main.rs
+++ tut_2/src/main.rs
@@ -8,18 +8,16 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    let num_1: f32 = 1.111111111111111;
-    println!("f32: {}", num_1 + 0.111111111111111);
-    let num_2: f64 = 1.111111111111111;
-    println!("f64: {}", num_2 + 0.111111111111111);
-    let num_3: u32 = 5;
-    let num_4: u32 = 4;
-    // Initialize the random number generator
-    let mut rng = rand::rng();
-
-    // Generate a random number between 1 and 100
-    let random_num: u32 = rng.random_range(1..=100);//this is an inclusive range
-    println!("Random number from 1 to 100 --> {}", random_num);
+    let age: u32 = 8;
+    if (age >= 1) && (age <= 18) {
+        println!("Important birthday!");
+    } else if (age == 21) || (age == 50) {
+        println!("Important birthday!");
+    } else if (age >= 65) {
+        println!("Important birthday!");
+    }else {
+        println!("Not an important birthday");
+    }
 }
 
 /*
