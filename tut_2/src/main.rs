c17cb9450a793cdbd651dc2e6e94edcf1d47f154
anxielray <anxielworld@gmail.com>
2025-03-29 07:45:16 -0400

    [Sizes]: {Update the tutorial 2 sourcefile to learn about the different types of sizes of variables in Rust programming language}

--- tut_2/src/main.rs
+++ tut_2/src/main.rs
@@ -8,11 +8,11 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    const ONE_MIL: u32 = 1_000_000;
-    const PI: f32 = 3.141592;
-    let age: &str = "47";
-    let mut age: u32 = age.trim().parse().expect("age wasn't assigned  a number");
-    //whatever  just did from above is called shadowing; using a variable more than once. and it is acceptable to use the same variable with different data types
-    age = age+1;
-    println!("I'm {} and I want to earn ${}", age, ONE_MIL);
+    println!("Max u32 : {}", u32::MAX);
+    println!("Max u64 : {}", u64::MAX);
+    println!("Max usize : {}", usize::MAX);
+    println!("Max u128 : {}", u128::MAX);
+    println!("Max f32 : {}", f32::MAX);
+    println!("Max f64 : {}", f64::MAX);
+
 }
